570747e86eeed7a0372d7c58ffd243ebb6179883
goya <goyasho0831@gmail.com>
2019-11-29 11:59:57 +0900

    java11 の新機能お試し（実装確認のため少しだけ）

---
+++ src/test/java/sample/version11/NewFeaturesTest.java
@@ -0,0 +1,24 @@
+package sample.version11;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+public class NewFeaturesTest {
+  @Test
+  void var() {
+    var str = "hoge";
+    var list = new ArrayList<String>();
+    assertTrue(str instanceof String);
+    assertTrue(list instanceof ArrayList);
+  }
+  
+  @Test
+  void strintRepeat() {
+    var str = "hoge".repeat(3);
+    assertEquals(str, "hogehogehoge");
+  }
+}
